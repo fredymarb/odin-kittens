2e64e86b3e6ac5a76e51b01b982b2f4f3c6329bc
fredymarb <29468132+fredymarb@users.noreply.github.com>
2026-03-26 16:49:00 +0000

    chore(dev): allow api.lvh.me host for local API testing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Allow local API subdomain for development
+  config.hosts << "api.lvh.me"
+
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
 
